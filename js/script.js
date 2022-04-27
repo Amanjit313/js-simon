eb134b8f20b038d8fc6dc1ed78ea5fd2df269f36
Amanjit313 <amanjitsingh313@gmail.com>
2022-04-27 09:24:42 +0200

    Simon Says - Da sistemare

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/* 
+  - Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
+
+  - Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+  - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+*/
+
+let number = [];
+let contatore = 0;
+let counter = 6;
+
+while (contatore < 5){
+  number = Math.floor(Math.random() * 10) + 1;
+  console.log(number);
+  contatore++;
+}
+
+console.log("-----------------------");
+
+const clock = setInterval(contoallarovescia, 1000);
+
+function contoallarovescia(){
+  counter--;
+  console.log(counter);
+  if(counter < 2){
+    clearInterval(clock);
+    console.log("Tempo scaduto!");
+  }
+}
