b9d49d30356d859c377461de14c07027053096da
Amanjit313 <amanjitsingh313@gmail.com>
2022-04-27 16:02:38 +0200

    Simon Says - Complete

--- js/script.js
+++ js/script.js
@@ -7,18 +7,20 @@
 
 */
 
-let number = [];
-let numberSelect = [];
+let numbers = []; // Array numeri random
+let numberSelect = []; // Array numeri selezionati
+let indovinaNumeri = []; // Array numeri combacianti
+
 let contatore = 0;
 let contatoreprompt = 0;
 let counter = 6;
 
 // Contatore per stampare i 5 numeri random da ricordare
 while (contatore < 5){
-  randomNumbers = Math.floor(Math.random() * 100) + 1;
+  randomNumber = Math.floor(Math.random() * 100) + 1;
   contatore++;
-  if(!number.includes(randomNumbers)){
-    number.push(randomNumbers);
+  if(!numbers.includes(randomNumber)){
+    numbers.push(randomNumber);
   }
 }
 
@@ -31,36 +33,42 @@ function contoallarovescia(){
   console.log(counter);
 
   // Stampo i numeri random
-  document.getElementById("numeri").innerHTML = "Hai" + " " + counter + " " + "secondi per ricordarti questi numeri:" + " " + number;
+  document.getElementById("numeri").innerHTML = "Hai" + " " + counter + " " + "secondi per ricordarti questi numeri:" + " " + numbers;
   
   if(counter < 1){
     clearInterval(clock);
-    clearInterval(number);
     console.log("Counter Fine!");
+    document.getElementById("numeri").innerHTML = "";
     aftercounter();
   }
 }
 
-console.log("Lista dei numeri casuali estratti" + " " + number);
+console.log("Lista dei numeri casuali estratti" + " " + numbers);
 console.log("------------------------");
 
 // I numeri da indovinare tramite i prompt dopo i 5 secondi
 function aftercounter(){
 
   while (contatoreprompt < 5){
-  let numberprompt  = parseInt (prompt("Inserisci i numeri ricordati da uno a uno"));
-  console.log("Numero scelto" + " " + numberprompt);
-  contatoreprompt++;
-  if(!numberSelect.includes(numberprompt)){
-    numberSelect.push(numberprompt);
-    console.log(numberSelect);
-    document.getElementById("numeriselezionati").innerHTML = "I numeri scelti:" + " " + numberSelect;
+    let numberprompt  = parseInt (prompt("Inserisci i numeri ricordati da uno a uno"));
+    console.log("Numero scelto" + " " + numberprompt);
+    contatoreprompt++;
+    if(!numberSelect.includes(numberprompt)){
+      numberSelect.push(numberprompt);
+      console.log(numberSelect);
+      document.getElementById("numeriselezionati").innerHTML = "I numeri scelti:" + " " + numberSelect;
+    }
+  } 
+
+  // Confronti dei numeri
+  for (let i = 0; i < numbers.length; i++){
+
+    if(numbers.includes(numberSelect[i])){
+      indovinaNumeri.push(numberSelect[i])
+      console.log("Numeri combacianti" + indovinaNumeri);
+      document.getElementById("numeriindovinati").innerHTML = "Ecco i numeri che hai indovinato" + " " + indovinaNumeri;
+    }
+
   }
-}
 
 }
-
-// Confronti dei numeri
-
-/* const numeriIndovinati = () */
-
